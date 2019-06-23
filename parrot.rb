4b7b38c8edc5ddfc3b71b48af9ad399e29f7ff0e
Chantel Lowe <chantellowe17@gmail.com>
2019-06-23 06:48:15 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string)
-  if parrot(string) == nil
+def parrot(phrase)
+  if parrot(phrase) == nil
     puts "Squawk!"
     "Squawk!"
   end
